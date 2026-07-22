8f2354f5f44d6f977ad0300a7258b09b00bd8dad
rie4649 <okm.co@icloud.com>
2026-07-22 13:35:21 +0900

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,47 @@
+const CACHE_NAME = "sharyo-daicho-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  // Firestoreへのリクエストはキャッシュせず常にネットワークから取得
+  if (event.request.url.includes("firestore.googleapis.com")) {
+    return;
+  }
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      return (
+        cached ||
+        fetch(event.request)
+          .then((response) => {
+            const clone = response.clone();
+            caches.open(CACHE_NAME).then((cache) => cache.put(event.request, clone));
+            return response;
+          })
+          .catch(() => cached)
+      );
+    })
+  );
+});
